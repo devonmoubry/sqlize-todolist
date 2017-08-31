e08dbeed64caab7b9ddb89bb16280b73078ef3ed
Devon Moubry <devon@moubry.com>
2017-08-31 15:28:22 -0500

    Render add delete update and complete functionality

--- app.js
+++ app.js
@@ -25,6 +25,68 @@ app.get("/todos", function(req, res) {
   });
 });
 
+app.post("/todos", function(req, res) {
+  console.log("req.body", req.body);
+  const todo = models.Todos.build({
+    title: req.body.todoTitle,
+    description: req.body.todoDetails
+  });
+  todo
+    .save()
+    .then(function() {
+      res.redirect("/todos");
+    })
+    .catch(function(err) {
+      res.render("todos", { errors: err });
+    });
+});
+
+app.post("/todos/:id/destroy", function(req, res) {
+  models.Todos
+    .destroy({
+      where: {
+        id: parseInt(req.params.id)
+      }
+    })
+    .then(function() {
+      res.redirect("/todos");
+    });
+});
+
+app.post("/todos/:id/complete", function(req, res) {
+  var rightNow = new Date();
+
+  models.Todos.findById(req.params.id).then(function(todo) {
+    todo.update({ completed: rightNow }).then(function() {
+      res.send("ok");
+    });
+  });
+});
+
+app.post("/todos/:id/undoComplete", function(req, res) {
+  models.Todos.findById(req.params.id).then(function(todo) {
+    todo.update({ completed: null }).then(function() {
+      res.send("ok");
+    });
+  });
+});
+
+app.get("/todos/:id/updatepage", function (req, res) {
+  models.Todos.findById(req.params.id).then(function (todo) {
+    console.log();
+    res.render('update', {todo: todo});
+  })
+})
+
+app.post("/todos/:id/update", function (req, res) {
+  models.Todos.findById(req.params.id).then(function (todo) {
+    todo.update({
+      title: req.body.todoTitle,
+      details: req.body.todoDetails
+    }).then(function () {
+      res.redirect("/todos")
+    })
+  })
 })
 
 app.listen(3000, function() {
