ddc85842a6e45ea90aad06c9b8fbf78552ed858c
Devon Moubry <devon@moubry.com>
2017-08-31 15:26:32 -0500

    Refactor

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-const express = require('express'),
-      mustacheExpress = require('mustache-express'),
-      bodyParser = require('body-parser'),
-      sequelize = require('sequelize')
-      models = require("./models");
+const express = require("express"),
+  mustacheExpress = require("mustache-express"),
+  bodyParser = require("body-parser"),
+  models = require("./models");
 
 const app = express();
 
-app.engine('mustache', mustacheExpress());
-app.set('views', './views');
-app.set('view engine', 'mustache')
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
 
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 
 app.get('/', function(req, res) {
     res.render("index");
 })
 
 app.listen(3000, function() {
-    console.log('Express running on http://localhost:3000/.')
+  console.log("Express running on http://localhost:3000/.");
 });
 
-process.on('SIGINT', function() {
+process.on("SIGINT", function() {
   console.log("\nshutting down");
-  const index = require('./models/index')
-  index.sequelize.close()
+  const index = require("./models/index");
+  index.sequelize.close();
 
- // give it a second
+  // give it a second
   setTimeout(function() {
-    console.log('process exit');
+    console.log("process exit");
     process.exit(0);
-  }, 1000)
+  }, 1000);
 });
